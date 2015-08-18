6921ea22a0a3cd5e2fdab68d0e1474e0de36ed2c
Nedearb <nedearb@gmail.com>
2015-08-18 15:56:22 -0700

    Games

---
+++ TFT_Pacman/TFT_Pacman.ino
@@ -0,0 +1,510 @@
+#include <TFT.h>
+#include <SPI.h>
+
+#define PIN_CS 10
+#define PIN_DC 9
+#define PIN_RESET 8
+
+#define PIN_BUTTON_STICK 2
+#define PIN_BUTTON_RIGHT 3
+#define PIN_BUTTON_UP 4
+#define PIN_BUTTON_DOWN 5
+#define PIN_BUTTON_LEFT 6
+
+#define PIN_AXIS_X A0
+#define PIN_AXIS_Y A1
+
+#define HEIGHT 128
+#define WIDTH 160
+
+TFT screen = TFT(PIN_CS, PIN_DC, PIN_RESET);
+
+#define COLOR_R_WALL 0
+#define COLOR_G_WALL 0
+#define COLOR_B_WALL 255
+
+#define COLOR_R_GATE 255
+#define COLOR_G_GATE 255
+#define COLOR_B_GATE 255
+
+#define COLOR_R_PELLET 255
+#define COLOR_G_PELLET 255
+#define COLOR_B_PELLET 255
+
+#define COLOR_R_POWER_PELLET 255
+#define COLOR_G_POWER_PELLET 255
+#define COLOR_B_POWER_PELLET 255
+
+#define COLOR_R_PACMAN 255
+#define COLOR_G_PACMAN 255
+#define COLOR_B_PACMAN 0
+
+#define TILE_EMPTY 0
+#define TILE_WALL 1
+#define TILE_GATE 2
+#define TILE_PELLET 3
+#define TILE_POWER_PELLET 4
+
+#define LEVEL_WIDTH 28
+#define LEVEL_HEIGHT 31
+
+#define JOYSTICK_BOUNDRY 128
+
+#define JOYSTICK_UPPER_BOUNDRY (512+JOYSTICK_BOUNDRY)
+#define JOYSTICK_LOWER_BOUNDRY (512-JOYSTICK_BOUNDRY)
+
+#define tile(x, y) grid[y][x]
+
+#define solid(x, y) ((tile(x, y)==TILE_WALL)||(tile(x, y)==TILE_GATE))
+
+#define DIR_LEFT 0
+#define DIR_RIGHT 1
+#define DIR_UP 2
+#define DIR_DOWN 3
+#define DIR_NONE 4
+
+#define GHOST_RED 0
+#define GHOST_BLUE 1
+#define GHOST_PINK 2
+#define GHOST_ORANGE 3
+
+#define SPEED_PACMAN 1
+
+byte grid[LEVEL_HEIGHT][LEVEL_WIDTH] = {
+{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+{1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 4, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 4, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 1},
+{1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 0, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{1, 1, 1, 1, 1, 1, 3, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 3, 1, 1, 1, 1, 1, 1},
+{0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0,  },
+{1, 1, 1, 1, 1, 1, 3, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 3, 1, 1, 1, 1, 1, 1},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{0, 0, 0, 0, 0, 1, 3, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 0, 0, 0, 0, 0},
+{1, 1, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 1, 1},
+{1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1},
+{1, 4, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 4, 1},
+{1, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 1},
+{1, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 1},
+{1, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 1, 1, 3, 3, 3, 3, 3, 3, 1},
+{1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1},
+{1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1},
+{1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1},
+{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+int tick = 0;
+
+byte pacX = 0;
+byte pacY = 0;
+byte pacLastX = 0;
+byte pacLastY = 0;
+byte pacDir = 0;
+byte pacFrame = 0;
+
+byte ghostX[4] = {0, 0, 0, 0};
+byte ghostY[4] = {0, 0, 0, 0};
+byte ghostLastX[4] = {0, 0, 0, 0};
+byte ghostLastY[4] = {0, 0, 0, 0};
+byte ghostDir[4] = {0, 0, 0, 0};
+byte ghostMode[4] = {0, 0, 0, 0};
+
+int score = 0;
+byte lives = 0;
+int timer = 0;
+long lastMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(PIN_BUTTON_STICK, INPUT);
+  digitalWrite(PIN_BUTTON_STICK, HIGH);
+  
+  pinMode(PIN_BUTTON_RIGHT, INPUT);
+  digitalWrite(PIN_BUTTON_RIGHT, HIGH);
+  
+  pinMode(PIN_BUTTON_UP, INPUT);
+  digitalWrite(PIN_BUTTON_UP, HIGH);
+  
+  pinMode(PIN_BUTTON_DOWN, INPUT);
+  digitalWrite(PIN_BUTTON_DOWN, HIGH);
+  
+  pinMode(PIN_BUTTON_LEFT, INPUT);
+  digitalWrite(PIN_BUTTON_LEFT, HIGH);
+  
+  screen.begin();
+  screen.background(0, 0, 0);
+  
+  
+  drawMap();
+  
+  score = 0;
+  lives = 3;
+  timer = 0;
+  
+  pacX = (13*4)+2;
+  pacY = 23*4;
+  pacDir = DIR_NONE;
+  pacLastX = pacX;
+  pacLastY = pacY;
+  pacFrame = 0;
+  
+  ghostX[GHOST_RED] = (13*4)+2;
+  ghostY[GHOST_RED] = 11*4;
+  
+  ghostX[GHOST_BLUE] = (11*4)+2;
+  ghostY[GHOST_BLUE] = 14*4;
+  
+  ghostX[GHOST_PINK] = (13*4)+2;
+  ghostY[GHOST_PINK] = 14*4;
+  
+  ghostX[GHOST_ORANGE] = (15*4)+2;
+  ghostY[GHOST_ORANGE] = 14*4;
+  
+  for(byte i=0;i<4;i++){
+    ghostLastX[i] = ghostX[i];
+    ghostLastY[i] = ghostY[i];
+    ghostDir[i] = DIR_NONE;
+    ghostScared[i] = false;
+  }
+}
+
+void loop() {
+  
+  short xAxis = analogRead(PIN_AXIS_X);
+  short yAxis = 1023-analogRead(PIN_AXIS_Y);
+
+  /*screen.stroke(0, 0, 0);
+  screen.line(WIDTH-1, HEIGHT-1, 0, HEIGHT-1);
+  screen.line(WIDTH-1, HEIGHT-1, WIDTH-1, 0);
+  screen.stroke(255, 0, 0);
+  screen.line(WIDTH-1, HEIGHT-1, map(xAxis, 0, 1023, 0, WIDTH-1), HEIGHT-1);
+  screen.line(WIDTH-1, HEIGHT-1, WIDTH-1, map(yAxis, 0, 1023, 0, HEIGHT-1));*/
+  
+  char inputX = xAxis > JOYSTICK_UPPER_BOUNDRY ? 1 : (xAxis < JOYSTICK_LOWER_BOUNDRY ? -1 : 0);
+  char inputY = yAxis > JOYSTICK_UPPER_BOUNDRY ? 1 : (yAxis < JOYSTICK_LOWER_BOUNDRY ? -1 : 0);
+  
+  
+  tick++;
+  
+  if((inputX == -1 || !digitalRead(PIN_BUTTON_LEFT)) && !solid((pacX-1) >> 2, pacY >> 2)){
+    pacDir = DIR_LEFT;
+  }else if((inputX == 1 || !digitalRead(PIN_BUTTON_RIGHT)) && !solid((pacX >> 2)+1, pacY >> 2)){
+    pacDir = DIR_RIGHT;
+  }else if((inputY == -1 || !digitalRead(PIN_BUTTON_UP)) && !solid(pacX >> 2, (pacY-1) >> 2)){
+    pacDir = DIR_UP;
+  }else if((inputY == 1 || !digitalRead(PIN_BUTTON_DOWN)) && !solid(pacX >> 2, (pacY >> 2)+1)){
+    pacDir = DIR_DOWN;
+  }
+  
+  delay(10);
+  
+  if(tick % 15 == 0){
+    pacFrame++;
+    if(pacFrame == 3){
+      pacFrame = 1;
+    }
+  }
+
+  //mode | 2 = (00 or 01) to (10 or 11)
+  //mode & 1 = (10 or 11) to (00 or 01)
+  
+  switch(timer){
+    case 0:{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] | 2;
+      }
+    }
+    case (7):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] & 1;
+      }
+    }
+    case (7+20):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] | 2;
+      }
+    }
+    case (7+20+7):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] & 1;
+      }
+    }
+    case (7+20+7+20):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] | 2;
+      }
+    }
+    case (7+20+7+20+5):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] & 1;
+      }
+    }
+    case (7+20+7+20+5+20):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] | 2;
+      }
+    }
+    case (7+20+7+20+5+20+5):{
+      for(byte i=0;i<4;i++){
+        ghostMode[i] = ghostMode[i] & 1;
+      }
+    }
+  }
+
+  if(millis() - lastMillis > 1000){
+    timer++;
+  }
+  
+  if(tick % 5 == 0){
+
+    if(pacX==0){
+      pacX = (LEVEL_WIDTH-1)*4;
+    }else if(pacX==(LEVEL_WIDTH-1)*4){
+      pacX = 0;
+    }
+    
+    drawTile(pacLastX >> 2, pacLastY >> 2);
+    drawTile((pacLastX >> 2)+1, pacLastY >> 2);
+    drawTile(pacLastX >> 2, (pacLastY >> 2)+1);
+    drawTile((pacLastX >> 2)+1, (pacLastY >> 2)+1);
+    
+    
+    for(byte i=0;i<4;i++){
+      drawTile(ghostLastX[i] >> 2, ghostLastY[i] >> 2);
+      drawTile((ghostLastX[i] >> 2)+1, ghostLastY[i] >> 2);
+      drawTile(ghostLastX[i] >> 2, (ghostLastY[i] >> 2)+1);
+      drawTile((ghostLastX[i] >> 2)+1, (ghostLastY[i] >> 2)+1);
+    }
+      
+    for(byte i=0;i<4;i++){
+      drawGhost(i);
+      ghostLastX[i] = ghostX[i];
+      ghostLastY[i] = ghostY[i];
+    }
+
+    
+    drawPacman();
+    pacLastX = pacX;
+    pacLastY = pacY;
+    
+  
+    if(pacDir == DIR_LEFT && !solid((pacX-1) >> 2, pacY >> 2)){
+      pacX -= SPEED_PACMAN;
+      pacY = (pacY >> 2) << 2;
+    }else if(pacDir == DIR_RIGHT && !solid((pacX >> 2)+1, pacY >> 2)){
+      pacX += SPEED_PACMAN;
+      pacY = (pacY >> 2) << 2;
+    }else if(pacDir == DIR_UP && !solid(pacX >> 2, (pacY-1) >> 2)){
+      pacY -= SPEED_PACMAN;
+      pacX = (pacX >> 2) << 2;
+    }else if(pacDir == DIR_DOWN && !solid(pacX >> 2, (pacY >> 2)+1)){
+      pacY += SPEED_PACMAN;
+      pacX = (pacX >> 2) << 2;
+    }
+
+    
+    
+    if(tile(pacLastX >> 2, pacLastY >> 2) == TILE_PELLET){
+      tile(pacLastX >> 2, pacLastY >> 2) = TILE_EMPTY;
+      score++;
+    }
+    
+    if(tile(pacLastX >> 2, pacLastY >> 2) == TILE_POWER_PELLET){
+      tile(pacLastX >> 2, pacLastY >> 2) = TILE_EMPTY;
+      
+      score += 10;
+    }
+  }
+  
+}
+
+void drawGhost(byte i){
+  screen.noStroke();
+  
+  if(ghostScared[i]){
+    screen.fill(0, 0, 255);
+  }else{
+    switch(i){
+      case GHOST_RED:{
+        screen.fill(255, 0, 0);
+        break;
+      }
+      case GHOST_BLUE:{
+        screen.fill(0, 255, 255);
+        break;
+      }
+      case GHOST_PINK:{
+        screen.fill(255, 127, 127);
+        break;
+      }
+      case GHOST_ORANGE:{
+        screen.fill(255, 127, 0);
+        break;
+      }
+    }
+  }
+  
+  screen.rect(ghostX[i], ghostY[i], 4, 4);
+  screen.stroke(0, 0, 0);
+  screen.point(ghostX[i], ghostY[i]);
+  screen.point(ghostX[i]+3, ghostY[i]);
+  screen.point(ghostX[i]+((tick>>2)&1), ghostY[i]+3);
+  screen.point(ghostX[i]+((tick>>2)&1)+2, ghostY[i]+3);
+  
+}
+
+void drawPacman(){
+  
+  screen.noStroke();
+  screen.fill(COLOR_R_PACMAN, COLOR_G_PACMAN, COLOR_B_PACMAN);
+  screen.rect(pacX, pacY, 4, 4);
+  screen.stroke(0, 0, 0);
+  screen.point(pacX, pacY);
+  screen.point(pacX+3, pacY);
+  screen.point(pacX, pacY+3);
+  screen.point(pacX+3, pacY+3);
+  
+  switch(pacFrame){
+    case 2:{
+      if(pacX > pacLastX){
+        screen.point(pacX+2, pacY+1);
+        screen.point(pacX+3, pacY+1);
+        screen.point(pacX+2, pacY+2);
+        screen.point(pacX+3, pacY+2);
+      }else if(pacX < pacLastX){
+        screen.point(pacX, pacY+1);
+        screen.point(pacX+1, pacY+1);
+        screen.point(pacX, pacY+2);
+        screen.point(pacX+1, pacY+2);
+      }else if(pacY > pacLastY){
+        screen.point(pacX+1, pacY+2);
+        screen.point(pacX+1, pacY+3);
+        screen.point(pacX+2, pacY+2);
+        screen.point(pacX+2, pacY+3);
+      }else if(pacY < pacLastY){
+        screen.point(pacX+1, pacY);
+        screen.point(pacX+1, pacY+1);
+        screen.point(pacX+2, pacY);
+        screen.point(pacX+2, pacY+1);
+      }
+    }
+  }
+}
+
+void drawMap(){
+  
+  for(byte i=0;i<LEVEL_WIDTH;i++){
+    for(byte j=0;j<LEVEL_HEIGHT;j++){
+      drawTile(i, j);
+    }
+  }
+  
+}
+
+void drawTile(byte x, byte y){
+
+  if(x >= LEVEL_WIDTH){
+    return;
+  }
+  
+  byte tile = tile(x, y);
+  byte tileLeft = x>0?tile(x-1, y):TILE_EMPTY;
+  byte tileRight = x<LEVEL_WIDTH-1?tile(x+1, y):TILE_EMPTY;
+  byte tileUp = y>0?tile(x, y-1):TILE_EMPTY;
+  byte tileDown = y<LEVEL_HEIGHT-1?tile(x, y+1):TILE_EMPTY;
+  
+  byte x4 = x*4;
+  byte y4 = y*4;
+  
+  
+  screen.noStroke();
+  screen.fill(0, 0, 0);
+  screen.rect(x4, y4, 4, 4);
+  
+  switch(tile){
+    case TILE_EMPTY:{
+      break;
+    }
+    case TILE_WALL:{
+      screen.stroke(COLOR_R_WALL, COLOR_G_WALL, COLOR_B_WALL);
+      if(tileLeft != TILE_WALL){
+          screen.point(x4, y4+1);
+          screen.point(x4, y4+2);
+          if(tileUp == TILE_WALL){
+            screen.point(x4, y4);
+          }
+          if(tileDown == TILE_WALL){
+            screen.point(x4, y4+3);
+          }
+      }
+      if(tileRight != TILE_WALL){
+          screen.point(x4+3, y4+1);
+          screen.point(x4+3, y4+2);
+          if(tileUp == TILE_WALL){
+            screen.point(x4+3, y4);
+          }
+          if(tileDown == TILE_WALL){
+            screen.point(x4+3, y4+3);
+          }
+      }
+      if(tileUp != TILE_WALL){
+          screen.point(x4+1, y4);
+          screen.point(x4+2, y4);
+          if(tileLeft == TILE_WALL){
+            screen.point(x4, y4);
+          }
+          if(tileRight == TILE_WALL){
+            screen.point(x4+3, y4);
+          }
+      }
+      if(tileDown != TILE_WALL){
+          screen.point(x4+1, y4+3);
+          screen.point(x4+2, y4+3);
+          if(tileLeft == TILE_WALL){
+            screen.point(x4, y4+3);
+          }
+          if(tileRight == TILE_WALL){
+            screen.point(x4+3, y4+3);
+          }
+      }
+      break;
+    }
+    case TILE_GATE:{
+      screen.noStroke();
+      screen.fill(COLOR_R_GATE, COLOR_G_GATE, COLOR_B_GATE);
+      screen.rect(x4, y4+1, 4, 2);
+      break;
+    }
+    case TILE_PELLET:{
+      screen.noStroke();
+      screen.fill(COLOR_R_PELLET, COLOR_G_PELLET, COLOR_B_PELLET);
+      screen.rect(x4+1, y4+1, 2, 2);
+      break;
+    }
+    case TILE_POWER_PELLET:{
+      screen.noStroke();
+      screen.fill(COLOR_R_POWER_PELLET, COLOR_G_POWER_PELLET, COLOR_B_POWER_PELLET);
+      screen.rect(x4, y4, 4, 4);
+      screen.stroke(0, 0, 0);
+      screen.point(x4, y4);
+      screen.point(x4+3, y4);
+      screen.point(x4, y4+3);
+      screen.point(x4+3, y4+3);
+      break;
+    }
+  }
+  
+}
+
---
+++ TFT_Tetris/TFT_Tetris.ino
@@ -0,0 +1,487 @@
+#include <TFT.h>
+#include <SPI.h>
+#include "pitches.h"
+
+#define CS   10
+#define DC   9
+#define RESET  8
+
+#define PIN_BUTTON_LEFT 7
+#define PIN_BUTTON_MIDDLE 6
+#define PIN_BUTTON_RIGHT 5
+
+#define PIN_SPEAKER 3
+
+#define BLOCK_SIZE 6
+
+#define X -256
+
+#define GRID_SKINNY 10
+#define GRID_LONG 22
+
+TFT screen = TFT(CS, DC, RESET);
+
+bool leftWasPressed, rightWasPressed, middleWasPressed;
+
+unsigned int tick = 0;
+unsigned int score = 0;
+unsigned int highScore = 0;
+
+short dropX = GRID_LONG;
+short dropY = GRID_SKINNY / 2;
+
+char dropIndex = 0;
+byte dropRotation = 0;
+
+char nextDrop = 0;
+
+short lastDropX = dropX;
+short lastDropY = dropY;
+byte lastDropRotation = dropRotation;
+
+char grid[GRID_LONG][GRID_SKINNY];
+
+short screenWidth, screenHeight, leftEdge, rightEdge, bottomEdge;
+
+#define startDropDelay 900
+short dropDelay = startDropDelay;
+
+unsigned long time;
+unsigned long lastTime;
+unsigned long lastNoteTime;
+
+#define pieceI 0
+#define pieceO 1
+#define pieceT 2
+#define pieceS 3
+#define pieceZ 4
+#define pieceJ 5
+#define pieceL 6
+
+char pieceList[4][7][8] = {
+  { //Rotated 0
+    { -1, 0, 0, 0, 1, 0, 2, 0}, //I SAME as 180
+    {0, 0, 1, 0, 0, 1, 1, 1}, //O SAME as 180
+    {0, -1, -1, 0, 0, 0, 1, 0}, //T
+    {0, 0, 1, 0, -1, 1, 0, 1}, //S SAME as 180
+    { -1, 0, 0, 0, 0, 1, 1, 1}, //Z SAME as 180
+    { -1, 0, -1, 1, 0, 1, 1, 1}, //J
+    {1, 0, -1, 1, 0, 1, 1, 1} //L
+  }, { //Rotated 90 Counter-Clockwise
+    {0, -1, 0, 0, 0, 1, 0, 2}, //I SAME as 270
+    {0, 0, 1, 0, 0, 1, 1, 1}, //O SAME as 270
+    {1, 0, 0, -1, 0, 0, 0, 1}, //T
+    {0, 0, 0, -1, 1, 1, 1, 0}, //S SAME as 270
+    {0, 1, 0, 0, 1, 0, 1, -1}, //Z SAME as 270
+    {0, -1, -1, -1, -1, 0, -1, 1}, //J
+    {0, 1, -1, -1, -1, 0, -1, 1} //L
+  }, { //Rotated 180 Counter-Clockwise
+    { -1, 0, 0, 0, 1, 0, 2, 0}, //I SAME as 0
+    {0, 0, 1, 0, 0, 1, 1, 1}, //O SAME as 0
+    {0, 1, 1, 0, 0, 0, -1, 0}, //T
+    {0, 0, 1, 0, -1, 1, 0, 1}, // S SAME as 0
+    { -1, 0, 0, 0, 0, 1, 1, 1}, // Z SAME as 0
+    {1, 0, 1, -1, 0, -1, -1, -1}, //J
+    { -1, 0, 1, -1, 0, -1, -1, -1} //L
+  }, { //Rotated 270 Counter-Clockwise
+    {0, -1, 0, 0, 0, 1, 0, 2}, //I SAME as 90
+    {0, 0, 1, 0, 0, 1, 1, 1}, //O SAME as 90
+    { -1, 0, 0, -1, 0, 0, 0, 1}, //T
+    {0, 0, 0, -1, 1, 1, 1, 0}, //S SAME as 90
+    {0, 1, 0, 0, 1, 0, 1, -1}, //Z SAME as 90
+    {0, 1, 1, 1, 1, 0, 1, -1}, //J
+    {0, -1, 1, 1, 1, 0, 1, -1} //L
+  }
+};
+
+#define noteCount 63
+short notes[noteCount] = {
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_E6,NOTE_E6, 
+NOTE_NONE, 
+NOTE_B5,NOTE_B5,  NOTE_C6,NOTE_C6,  NOTE_D6,NOTE_D6,
+NOTE_NONE,
+NOTE_C6,NOTE_C6,  NOTE_B5,NOTE_B5,  NOTE_A5,NOTE_A5,
+NOTE_NONE,
+NOTE_A5,NOTE_A5,  NOTE_C6,NOTE_C6,  NOTE_E6,NOTE_E6,
+NOTE_NONE,
+NOTE_D6,NOTE_D6,  NOTE_C6,NOTE_C6,  NOTE_B5,NOTE_B5,
+NOTE_NONE,
+NOTE_B5,NOTE_B5,  NOTE_C6,NOTE_C6,  NOTE_D6,NOTE_D6,  NOTE_E6,NOTE_E6,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_C6,NOTE_C6,NOTE_C6,  NOTE_NONE,  
+NOTE_A5,NOTE_A5,NOTE_A5,  NOTE_NONE,  
+NOTE_A5,NOTE_A5,NOTE_A5,  NOTE_NONE,  
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+NOTE_NONE,
+
+};
+short toneDelay = 100;
+
+
+byte colorsR[7] = {0  , 255, 255, 0  , 255, 0  , 255};
+byte colorsG[7] = {255, 255, 0  , 255, 0  , 0  , 127};
+byte colorsB[7] = {255, 0  , 255, 0  , 0  , 255, 0  };
+
+void setup() {
+  screen.begin();
+  screen.background(0, 0, 0);
+  Serial.begin(9600);
+  screenWidth = screen.width();
+  screenHeight = screen.height();
+
+  pinMode(PIN_BUTTON_LEFT, INPUT);
+  pinMode(PIN_BUTTON_MIDDLE, INPUT);
+  pinMode(PIN_BUTTON_RIGHT, INPUT);
+
+  for (short i = 0; i < GRID_LONG; i++) {
+    for (short j = 0; j < GRID_SKINNY; j++) {
+      grid[i][j] = -1;
+    }
+  }
+
+  randomSeed(analogRead(A0));
+
+  dropIndex = random(0, 7);
+  nextDrop = random(0, 7);
+
+  leftEdge = (screenHeight - (GRID_SKINNY * BLOCK_SIZE)) / 2;
+  rightEdge = screenHeight - leftEdge;
+  bottomEdge = screenWidth - (GRID_LONG * BLOCK_SIZE);
+
+
+  drawBG();
+
+}
+
+void loop() {
+  
+  bool changed = false;
+
+  ///////////////////////////////////////////////////////
+  if (digitalRead(PIN_BUTTON_LEFT)) {
+    if (!leftWasPressed) {
+      changed = true;
+      dropY--;
+      if (dropCollidesAnywhere()) {
+        dropY++;
+      }
+    }
+  }
+  leftWasPressed = digitalRead(PIN_BUTTON_LEFT);
+
+  if (digitalRead(PIN_BUTTON_RIGHT)) {
+    if (!rightWasPressed) {
+      changed = true;
+      dropY++;
+      if (dropCollidesAnywhere()) {
+        dropY--;
+      }
+    }
+  }
+  rightWasPressed = digitalRead(PIN_BUTTON_RIGHT);
+
+  if (digitalRead(PIN_BUTTON_MIDDLE)) {
+    if (!middleWasPressed) {
+      changed = true;
+      dropRotation++;
+      if (dropRotation > 3) {
+        dropRotation = 0;
+      }
+      if (dropCollidesAnywhere()) {
+        dropRotation--;
+        if (dropRotation < 0) {
+          dropRotation = 3;
+        }
+      }
+    }
+  }
+  middleWasPressed = digitalRead(PIN_BUTTON_MIDDLE);
+  ///////////////////////////////////////////////////////
+  
+  time = millis();
+  
+  bool newDrop = false;
+
+  short x = bottomEdge;
+  short y = leftEdge;
+  
+  if((time - lastNoteTime) >= toneDelay){
+    tick++;
+    if(notes[tick%noteCount] == NOTE_NONE){
+      noTone(PIN_SPEAKER);
+    }else{
+      tone(PIN_SPEAKER, notes[tick%noteCount]);
+    }
+    lastNoteTime = time;
+  }
+  
+  
+
+  if ((time - lastTime) >= dropDelay) {
+    
+    lastTime = time;
+    
+    dropX--;
+    
+    changed = true;
+
+    //delay(10);
+
+
+    if (dropCollidesOnBottom()) {
+    
+      if(dropDelay > 100){
+        dropDelay -= 1;
+      }
+      
+      if (dropX + 1 == GRID_LONG) {
+        dropX = GRID_LONG;
+        dropY = GRID_SKINNY / 2;
+        dropIndex = random(0, 7);
+        nextDrop = random(0, 7);
+        dropRotation = 0;
+        drawBG();
+        for (short i = 0; i < GRID_LONG; i++) {
+          for (short j = 0; j < GRID_SKINNY; j++) {
+            grid[i][j] = -1;
+          }
+        }
+        highScore = max(highScore, score);
+        score = 0;
+        dropDelay = startDropDelay;
+
+        screen.fill(0, 0, 0);
+        screen.rect(x, y, screenWidth - bottomEdge, rightEdge - leftEdge);
+        return;
+      }
+      dropX++;
+      placeDrop();
+
+      dropX = GRID_LONG;
+      dropY = GRID_SKINNY / 2;
+
+      //lastDropX = dropX;
+      //lastDropY = dropY;
+      //lastDropRotation = dropRotation;
+
+      dropIndex = nextDrop;
+      nextDrop = random(0, 7);
+      dropRotation = 0;
+
+      drawBG();
+      //screen.fill(colorsR[nextDrop], colorsG[nextDrop], colorsB[nextDrop]);
+      //drawPiece(screenWidth-(4*BLOCK_SIZE), (2*BLOCK_SIZE), nextDrop, 0);
+
+      newDrop = true;
+
+      screen.fill(0, 0, 0);
+      screen.rect(x, y, screenWidth - bottomEdge, rightEdge - leftEdge);
+    }
+
+    byte rows[22];
+    bool rowDone = false;
+
+    for (short i = 0; i < GRID_LONG; i++) {
+      rows[i] = 0;
+      for (short j = 0; j < GRID_SKINNY; j++) {
+        char pieceIndex = grid[i][j];
+        if (pieceIndex > -1) {
+          screen.fill(colorsR[pieceIndex], colorsG[pieceIndex], colorsB[pieceIndex]);
+          screen.rect(x + (i * BLOCK_SIZE), y + (j * BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
+          rows[i]++;
+        }
+      }
+      if (rows[i] == 10) {
+        rowDone = true;
+      }
+    }
+
+    if (rowDone) {
+      for (byte i = 0; i < 3; i++) {
+        drawNeededRowsWhite(x, y, rows);
+        delay(200);
+        drawNeededRowsNormal(x, y, rows);
+        delay(200);
+      }
+      removeNeededRows(rows);
+
+      screen.fill(0, 0, 0);
+      screen.rect(x, y, screenWidth - bottomEdge, rightEdge - leftEdge);
+
+      drawBG();
+    }
+    
+  }
+  if(changed) {
+
+    if (!newDrop) {
+      screen.fill(0, 0, 0);
+      drawPiece(x + (lastDropX * BLOCK_SIZE), y + (lastDropY * BLOCK_SIZE), dropIndex, lastDropRotation);
+    }
+
+    screen.fill(colorsR[dropIndex], colorsG[dropIndex], colorsB[dropIndex]);
+    drawPiece(x + (dropX * BLOCK_SIZE), y + (dropY * BLOCK_SIZE), dropIndex, dropRotation);
+
+    lastDropX = dropX;
+    lastDropY = dropY;
+    lastDropRotation = dropRotation;
+
+
+
+  }
+}
+
+void removeNeededRows(byte rows[22]) {
+
+  for (byte i = 0; i < GRID_LONG; i++) {
+    if (rows[i] == GRID_SKINNY) {
+      for (byte j = 0; j < GRID_SKINNY; j++) {
+        grid[i][j] = -1;
+      }
+    }
+  }
+
+  bool rieatoain = false;
+
+  do {
+    rieatoain = false;
+    byte row = 0;
+    short x = 0;
+    for (byte i = 0; i < GRID_LONG; i++) {
+      for (byte j = 0; j < GRID_SKINNY; j++) {
+        char pieceIndex = grid[i][j];
+        if (grid[i][j] == -1 && rows[i] == GRID_SKINNY) {
+          row++;
+        }
+      }
+      if (row == 10) {
+        x = i;
+        break;
+      }
+    }
+
+    if (row == 10) {
+      rieatoain = true;
+      for (byte i = x; i < GRID_LONG - 1; i++) {
+        for (byte j = 0; j < GRID_SKINNY; j++) {
+          grid[i][j] = grid[i + 1][j];
+        }
+      }
+      score++;
+
+    }
+
+  } while (rieatoain);
+
+
+
+}
+
+void drawNeededRowsWhite(short x, short y, byte rows[22]) {
+  screen.fill(255, 255, 255);
+  for (byte i = 0; i < GRID_LONG; i++) {
+    if (rows[i] == GRID_SKINNY) {
+      screen.rect(x + (i * BLOCK_SIZE), y, BLOCK_SIZE, BLOCK_SIZE * 10);
+    }
+  }
+}
+
+void drawNeededRowsNormal(short x, short y, byte rows[22]) {
+  for (byte i = 0; i < GRID_LONG; i++) {
+    for (short j = 0; j < GRID_SKINNY; j++) {
+      char pieceIndex = grid[i][j];
+      if (pieceIndex > -1) {
+        screen.fill(colorsR[pieceIndex], colorsG[pieceIndex], colorsB[pieceIndex]);
+        screen.rect(x + (i * BLOCK_SIZE), y + (j * BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE);
+      }
+    }
+  }
+}
+
+void placeDrop() {
+  for (short i = 0; i < 8; i += 2) {
+    grid[(dropX + (pieceList[dropRotation][dropIndex][i]))][(dropY + (pieceList[dropRotation][dropIndex][i + 1]))] = dropIndex;
+  }
+}
+
+bool dropCollidesOnBottom() {
+  bool c = false;
+  for (short i = 0; i < GRID_LONG && !c; i++) {
+    for (short j = 0; j < GRID_SKINNY && !c; j++) {
+      for (short k = 0; k < 8 && !c; k += 2) {
+        if (gridOcupied(dropX + pieceList[dropRotation][dropIndex][k], dropY + pieceList[dropRotation][dropIndex][k + 1]) || dropX + pieceList[dropRotation][dropIndex][k] < 0) {
+          c = true;
+        }
+      }
+    }
+  }
+  return c;
+}
+
+bool gridOcupied(short x, short y) {
+  if (x < 0 || y < 0 || x >= GRID_LONG || y >= GRID_SKINNY) {
+    return false;
+  }
+  return grid[x][y] > -1;
+}
+
+bool dropCollidesAnywhere() {
+  bool c = false;
+  for (short i = 0; i < GRID_LONG && !c; i++) {
+    for (short j = 0; j < GRID_SKINNY && !c; j++) {
+      for (short k = 0; k < 8 && !c; k += 2) {
+        if (gridOcupiedOrOutOfBounds(dropX + pieceList[dropRotation][dropIndex][k], dropY + pieceList[dropRotation][dropIndex][k + 1])) {
+          c = true;
+        }
+      }
+    }
+  }
+  return c;
+}
+
+bool gridOcupiedOrOutOfBounds(short x, short y) {
+  if (x < 0 || y < 0 || x >= GRID_LONG || y >= GRID_SKINNY) {
+    return true;
+  }
+  return grid[x][y] > -1;
+}
+
+void drawBG() {
+  screen.fill(64, 64, 64);
+  screen.rect(0, 0, screenWidth, leftEdge);
+  screen.rect(0, rightEdge, screenWidth, screenWidth - rightEdge);
+  screen.rect(0, 0, bottomEdge, screenHeight);
+  screen.noFill();
+  screen.stroke(127, 127, 127);
+  screen.rect(bottomEdge - 1, leftEdge - 1, screenWidth - bottomEdge + 2, rightEdge - leftEdge + 2);
+  screen.noStroke();
+
+  screen.fill(colorsR[nextDrop], colorsG[nextDrop], colorsB[nextDrop]);
+  drawPiece(screenWidth - (4 * BLOCK_SIZE), (2 * BLOCK_SIZE), nextDrop, 0);
+
+  screen.stroke(255, 255, 255);
+  screen.text(String(score).c_str(), 2, 2);
+  if (score != highScore) {
+    screen.text("Highscore", 2, screenHeight - 20);
+    screen.text(String(highScore).c_str(), 2, screenHeight - 10);
+  }
+  screen.noStroke();
+
+}
+
+void drawPiece(short x, short y, char pieceIndex, byte rotation) {
+  for (short i = 0; i < 8; i += 2) {
+    screen.rect((x + (pieceList[rotation][pieceIndex][i]*BLOCK_SIZE)), (y + (pieceList[rotation][pieceIndex][i + 1]*BLOCK_SIZE)), BLOCK_SIZE, BLOCK_SIZE);
+  }
+  //screen.fill(127, 127, 127);
+  //screen.rect(x, y, BLOCK_SIZE/2, BLOCK_SIZE/2);
+}
